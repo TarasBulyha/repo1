79ba132c687467564f37443b409007ccd152141e
n.bulyga <n.bulyga@bankvostok.com.ua>
2020-07-23 15:35:34 +0300

    Try to merge it.

---
+++ Try to merge it/HomeController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Repo1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+    }
+}
